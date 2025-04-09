34b3ec2b03a5647033af1852dbf1f45b285f3b9c
hosna <hosna@kth.se>
2025-04-09 13:59:45 +0200

    Displays the carAd model
    The texture cannot load!

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,22 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  assetsInclude: ['**/*.glb', '**/*.gltf', '**/*.jpg', '**/*.png'],
+  publicDir: 'public',
   plugins: [react()],
   server: {
-    allowedHosts: ["organ-audio-orientation-occupations.trycloudflare.com"],
+    allowedHosts: ["div-sporting-arg-philip.trycloudflare.com"],
     cors: true, // Enable CORS for development
   },
   build: {
     assetsInlineLimit: 0, // Ensure images are processed as assets rather than inlined
+  },
+  optimizeDeps: {
+    include: ['three', 'three/examples/jsm/loaders/GLTFLoader'],
+  },
+  resolve: {
+    alias: {
+      three: 'three',
+    },
   },
 });
